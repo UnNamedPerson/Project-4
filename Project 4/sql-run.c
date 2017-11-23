0b6543de40f213f501313ef5ba7a52f193daa96e
Islomzhan Akhmedov <ahmedovislam00@gmail.com>
2017-11-23 11:00:59 -0500

    added delete function
    Output is:
    
    	Inserting Database
    
    Hash Table		Lists
    HASH[0]		-> {|"1232"| |"sala"| |"omq"|}
    HASH[1]		-> |"(null)"|
    HASH[2]		-> {|"1234"| |"anyd"| |"one"|}	-> {|"3421"| |"insa"| |"ofa"|}
    HASH[3]		-> {|"5678"| |"live"| |"edw"|}	-> {|"2135"| |"icid"| |"dar"|}
    -> {|"4322"| |"iyer"| |"mon"|}
    HASH[4]		-> {|"8784"| |"chfd"| |"roy"|}
    
    	Deleting Some Tuples
    
    Hash Table		Lists
    HASH[0]		-> |"(null)"|
    HASH[1]		-> |"(null)"|
    HASH[2]		-> {|"3421"| |"insa"| |"ofa"|}
    HASH[3]		-> {|"2135"| |"icid"| |"dar"|}	-> {|"4322"| |"iyer"| |"mon"|}
    HASH[4]		-> {|"8784"| |"chfd"| |"roy"|}

--- Project 4/sql-run.c
+++ Project 4/sql-run.c
@@ -23,6 +23,7 @@ struct CELL {
 
 typedef LIST HASHTABLE[B*6];
 
+// Hashing Function
 int h(ETYPE x)
 {
     int i, sum;
@@ -33,39 +34,25 @@ int h(ETYPE x)
     return sum % B;
 }
 
-void bucketInsert(ETYPE x, ETYPE y, ETYPE z,LIST *pL)
-{
-    if ((*pL) == NULL) {
-        (*pL) = (LIST) malloc(sizeof(struct CELL));
-        strcpy((*pL)->element, x);
-        strcpy((*pL)->element1, y);
-        strcpy((*pL)->element2, z);
-        (*pL)->next = NULL;
-    }
-    else
-        bucketInsert(x,y,z,&((*pL)->next));
-}
-
-void insert(ETYPE x, ETYPE y, ETYPE z,HASHTABLE H)
-{
-    bucketInsert(x,y,z,&(H[h(x)]));
-}
+// Display Functions and other methods that're needed for display functions
 int countNodes(LIST start){
-    
-    LIST p;
     int i = 0;
     if (start == NULL){
-        //printf("As nodes are 0, the list is empty\n");
-        return 0;
+        return i; //As nodes are 0, the list is empty
+    } else {
+        LIST p; p=start;
+        while (p!=NULL){
+            i++;
+            p=p->next;
+        }
+        return i;
     }
-    p=start;
-    while (p!=NULL){ i++; p=p->next;}
-    return i;
 }
+
 void displayCells(LIST start, int numnodes){
-    
-    LIST p = start;
-    for (int i = 0; i<(numnodes-1); i++) {
+   
+    LIST p; p=start;
+    for (int i = 0; i<(numnodes-1); i++){
         p=p->next;
         printf("\t-> {|\"%s\"| |\"%s\"| |\"%s\"|}", p->element,p->element1,p->element2);
     }
@@ -90,19 +77,71 @@ void displayRelations(HASHTABLE temp){
     printf("\n");
 }
 
+// Insert Functions
+void bucketInsert(ETYPE x, ETYPE y, ETYPE z,LIST *pL)
+{
+    if ((*pL) == NULL) {
+        (*pL) = (LIST) malloc(sizeof(struct CELL));
+        strcpy((*pL)->element, x);
+        strcpy((*pL)->element1, y);
+        strcpy((*pL)->element2, z);
+        (*pL)->next = NULL;
+    } else if (strcmp((*pL)->element, x))
+        bucketInsert(x,y,z,&((*pL)->next));
+    //it works also without else if statement -> in this way:
+    // else {bucketInsert(x,y,z,&((*pL)->next));}
+    //but book chose the first way
+    
+}
+void insert(ETYPE x, ETYPE y, ETYPE z,HASHTABLE H)
+{
+    bucketInsert(x,y,z,&(H[h(x)]));
+}
+
+// Delete Functions
+
+//if((*pL)->element!=x || (*pL)->element1!=y ||(*pL)->element2!=z){
+//still didn't get it that why  above code works it should be '==' and not '!='
+//as if it's '!=' doesn't make sense as if it's equal to x,y,or z then we should perform that remove operation
+//but if we put '==' it doesn't work, why???
+
+void bucketDelete(ETYPE x, ETYPE y, ETYPE z,LIST *pL)
+{
+    if ((*pL) != NULL) {
+        if(strcmp((*pL)->element, x)==0 || strcmp((*pL)->element1, y)==0 ||strcmp((*pL)->element2, z)==0){
+            (*pL)=(*pL)->next;
+        } else {
+            bucketDelete(x, y, z, &((*pL)->next));
+        }
+    }
+}
+void delete(ETYPE x, ETYPE y, ETYPE z,HASHTABLE S)
+{
+    bucketDelete(x,y,z,&(S[h(x)]));
+}
+
 int main(int argc, const char * argv[]) {
     HASHTABLE temp;
     
-    insert("anyone",    "any",   "one",  temp);
-    insert("lived",     "live",  "ed",   temp);
-    insert("in",        "icida", "dar",  temp);
-    insert("a",         "sal",   "om",   temp);
-    insert("pretty",    "ch",    "royli",temp);
-    insert("how",       "ins",   "of",   temp);
-    insert("town",      "iy",    "mon",  temp);
+    printf("\n\tInserting Database\n");
+
+    insert("1234",  "anyd", "one",  temp);
+    insert("5678",  "live", "edw",  temp);
+    insert("2135",  "icid", "dar",  temp);
+    insert("1232",  "sala", "omq",  temp);
+    insert("8784",  "chfd", "roy",  temp);
+    insert("3421",  "insa", "ofa",  temp);
+    insert("4322",  "iyer", "mon",  temp);
     
+    displayRelations(temp);
+    
+    printf("\n\tDeleting Some Tuples\n");
+    
+    delete("1232",  "sala", "omq",  temp);
+    delete("3421",  "insa", "ofa",  temp);
+    delete("2135",  "icid", "dar",  temp);
+
     displayRelations(temp);
     
     return 0;
 }
-
