d1497be9985794cee681742f489d6c5a56167749
Islomzhan Akhmedov <ahmedovislam00@gmail.com>
2017-11-25 11:02:57 -0500

    made more readable

--- Project 4/sql-run.c
+++ Project 4/sql-run.c
@@ -128,12 +128,12 @@ void lookup(ETYPE x, ETYPE y, ETYPE z,HASHTABLE L){
 
 }
 
-void saveDatabase(HASHTABLE temp){
+void saveDatabase(HASHTABLE temp, char *new_name){
 // create a FILE typed pointer
     FILE *file_pointer;
 
 // open/create the txt file for saving the database
-    file_pointer = fopen("check_data.txt","a+");
+    file_pointer = fopen(new_name,"a+");
 
 // write to the file
     LIST p;
@@ -148,146 +148,6 @@ void saveDatabase(HASHTABLE temp){
     fclose(file_pointer);
 }
 
-//char ch;
-//char word1[32];
-//char word2[32];
-//char word3[32];
-//
-//void loadDatabase(HASHTABLE temp){
-//    // create a FILE typed pointer
-//    FILE *file_pointer;
-//
-//    // open/create the txt file for reading/loading the database
-//    file_pointer = fopen("check_data.txt","r");
-//
-//    while ((ch = getc(file_pointer)) != EOF){
-//        int wordCounter = 0;
-//        while(ch != '\n'){
-//            char word_loop[32];        //islam \t 123412 \t 234123
-//            for(int i = 0; ch != '\t' ; i++){
-//                word_loop[i] = ch;
-//            }
-//            if(wordCounter == 0){
-//                strcpy(word1, word_loop);
-//                wordCounter++;
-//                ++ch;
-//            }
-//            if(wordCounter == 1){
-//                strcpy(word2, word_loop);
-//                wordCounter++;
-//                ++ch;
-//            }
-//            if(wordCounter == 2){
-//                strcpy(word3, word_loop);
-//                insert(word1, word2, word3, temp);
-//            }
-//        }
-//    }
-//}
-
-
-
-//void loadDatabase(HASHTABLE temp){
-//    // create a FILE typed pointer
-//    FILE *file_pointer;
-//
-//    // open/create the txt file for reading/loading the database
-//    file_pointer = fopen("check_data.txt","r");
-//
-//    while ((ch = getc(file_pointer)) != EOF){
-//            char word_loop[32];
-//            char *x[4];
-//            //word_loop = 3321 \t asds \t asw\n
-//            for(int i = 0; ch != '\n' ; i++){
-//                word_loop[i] = ch;
-//            }
-//            if (ch=='\n') {
-//                char *p;
-//                p = strtok(word_loop,"\t");
-//                int i = 0;
-//                x[0] = p;
-//
-//                while (strcmp(p,"\n")!=0) {
-//                    p = strtok(word_loop,"\t");
-//                    i++;
-//                    x[i]=p;
-//                    insert(x[1], x[2], x[3], temp);
-//                }
-//
-//             }
-//        ch++;
-//    }
-//}
-
-
-//void loadDatabase(HASHTABLE temp){
-//    // create a FILE typed pointer
-//    FILE *file_pointer;
-//
-//    // open/create the txt file for reading/loading the database
-//    file_pointer = fopen("check_data.txt","r");
-//    char ch;
-//    while ((ch = getc(file_pointer)) != EOF){
-//
-//
-//        char str[] = "this, is the string - I want to parse";
-//        char delim[] = " ,-";
-//        char* token;
-//
-//        for (token = strtok(str, delim); token; token = strtok(NULL, delim))
-//        {
-//            printf("token=%s\n", token);
-//        }
-//
-//        char word_loop[32];
-//        char *x[4];
-//        //word_loop = 3321 \t asds \t asw\n
-//        for(int i = 0; ch != '\n' ; i++){
-//            word_loop[i] = ch;
-//        }
-//        if (ch=='\n') {
-//            char *p;
-//            p = strtok(word_loop,"\t");
-//            int i = 0;
-//            x[0] = p;
-//
-//            while (strcmp(p,"\n")!=0) {
-//                p = strtok(word_loop,"\t");
-//                i++;
-//                x[i]=p;
-//                insert(x[1], x[2], x[3], temp);
-//            }
-//
-//        }
-//        ch++;
-//    }
-//}
-
-//
-//void printData(){
-//    char * buffer = 0;
-//    long length;
-//    FILE * f = fopen ("check_data.txt", "rb");
-//
-//    if (f)
-//    {
-//        fseek (f, 0, SEEK_END);
-//        length = ftell (f);
-//        fseek (f, 0, SEEK_SET);
-//        buffer = malloc (length);
-//
-//        if (buffer)
-//        {
-//            fread (buffer, 1, length, f);
-//        }
-//        fclose (f);
-//    }
-//
-//    if (buffer)
-//    {
-//        printf("%s", buffer);
-//    }
-//}
 void loadDatabase(char *filename, HASHTABLE temp)
 {
     // loads the database
@@ -304,97 +164,246 @@ void loadDatabase(char *filename, HASHTABLE temp)
     return;
 }
 
-//void parseToTokens(char str[], char delim[]){
-//    char* token;
-//    int i = 0;
-//    printf("The number of lines are %d\n", countlines("check_data.txt"));
-//    char *arr[countlines(str)];
-//
-//    for (token = strtok(str, delim); token; token = strtok(NULL, delim))
-//    {
-//        arr[i]=token;
-//        printf("token %d  is %s\n", i, token);
-//        i++;
-//    }
-//
-////    for (int i=0; i<countlines(str); i++) {
-////        printf("asdfasfsaf");
-//        printf("The array %d is %s\n", i, arr[1]);
-////}
-//}
-////void justTokens(char str[], char delim[]){
-////    char * pch;
-////    pch = strtok (str,delim);
-////    while (pch != NULL)
-////    {
-////        parseToTokens(pch, "\t");
-////        printf ("sente %s\n",pch);
-////        pch = strtok (NULL, delim);
-////    }
-////
-////}
-
-
-//
-//void printData(){
-//    char * buffer = 0;
-//    long length;
-//    FILE * f = fopen ("check_data.txt", "rb");
-//
-//    if (f){
-//        fseek (f, 0, SEEK_END);
-//        length = ftell (f);
-//        fseek (f, 0, SEEK_SET);
-//        buffer = malloc (length);
-//
-//        if (buffer) {
-//            fread (buffer, 1, length, f);
-//        }
-//        fclose (f);
-//
-//    }
-//    if (buffer){
-//        printf("TEXT in BUFFER\n");
-//        printf("%s", buffer);
-//        printf("\n\n");
-//        parseToTokens(buffer, "\t\n");
-//    }
-//}
-
-
-
 int main(int argc, const char * argv[]) {
-      HASHTABLE temp;
-//
-//    printf("\n\tInserting Database\n");
+    HASHTABLE temp;
+    char *name_of_database = "save_database_1.txt";
 
-//    insert("1234",  "anyd", "one",  temp);
-//    insert("5678",  "live", "edw",  temp);
-//    insert("2135",  "icid", "dar",  temp);
-//    insert("1232",  "sala", "omq",  temp);
-//    insert("8784",  "chfd", "roy",  temp);
-//    insert("3421",  "insa", "ofa",  temp);
-//    insert("4322",  "iyer", "mon",  temp);
+
+    //inserts into the database
+    printf("Inserting Database\n");
+    insert("1234",  "anyd", "one",  temp);
+    insert("5678",  "live", "edw",  temp);
+    insert("2135",  "icid", "dar",  temp);
+    insert("1232",  "sala", "omq",  temp);
+    insert("8784",  "chfd", "roy",  temp);
+    insert("3421",  "insa", "ofa",  temp);
+    insert("4322",  "iyer", "mon",  temp);
+
+    //displayRelations(temp);
+
+    //saves the database
+    saveDatabase(temp,name_of_database);
     
-      loadDatabase("check_data.txt", temp);
-      displayRelations(temp);
+/*** Make sure! You have already called "insert" & "saveDatabase" functions before loading database ***/
+    //loads the database
+    loadDatabase(name_of_database, temp);
+    displayRelations(temp);
     
-//    loadDatabase(temp);
-//
-//    displayRelations(temp);
-//
-//    saveDatabase(temp);
-//
-//    printf("\n\tDeleting Some Tuples\n");
-//
-//    delete("3421",  "insa", "ofa",  temp);
-//    delete("4322",  "iyer", "mon",  temp);
-//
-//    displayRelations(temp);
-//
-//    lookup("8784",  "*", "*",  temp);
-//    lookup("2135",  "icid", "*",  temp);
-//    lookup("1232",  "*", "omq",  temp);
+    //deletes some tuples
+    printf("\nDeleting Some Tuples\n");
+    delete("3421",  "insa", "ofa",  temp);
+    delete("4322",  "iyer", "mon",  temp);
+
+    displayRelations(temp);
+
+    //lookups some tuples
+    lookup("8784",  "*", "*",  temp);
+    lookup("2135",  "icid", "*",  temp);
+    lookup("1232",  "*", "omq",  temp);
     
     return 0;
 }
+
+/*
+ // ******CAN BE REMOVED******
+ 
+ char ch;
+ char word1[32];
+ char word2[32];
+ char word3[32];
+ 
+ void loadDatabase(HASHTABLE temp){
+ // create a FILE typed pointer
+ FILE *file_pointer;
+ 
+ // open/create the txt file for reading/loading the database
+ file_pointer = fopen("check_data.txt","r");
+ 
+ while ((ch = getc(file_pointer)) != EOF){
+ int wordCounter = 0;
+ while(ch != '\n'){
+ char word_loop[32];        //islam \t 123412 \t 234123
+ for(int i = 0; ch != '\t' ; i++){
+ word_loop[i] = ch;
+ }
+ if(wordCounter == 0){
+ strcpy(word1, word_loop);
+ wordCounter++;
+ ++ch;
+ }
+ if(wordCounter == 1){
+ strcpy(word2, word_loop);
+ wordCounter++;
+ ++ch;
+ }
+ if(wordCounter == 2){
+ strcpy(word3, word_loop);
+ insert(word1, word2, word3, temp);
+ }
+ }
+ }
+ }
+ 
+ 
+ 
+ void loadDatabase(HASHTABLE temp){
+ // create a FILE typed pointer
+ FILE *file_pointer;
+ 
+ // open/create the txt file for reading/loading the database
+ file_pointer = fopen("check_data.txt","r");
+ 
+ while ((ch = getc(file_pointer)) != EOF){
+ char word_loop[32];
+ char *x[4];
+ //word_loop = 3321 \t asds \t asw\n
+ for(int i = 0; ch != '\n' ; i++){
+ word_loop[i] = ch;
+ }
+ if (ch=='\n') {
+ char *p;
+ p = strtok(word_loop,"\t");
+ int i = 0;
+ x[0] = p;
+ 
+ while (strcmp(p,"\n")!=0) {
+ p = strtok(word_loop,"\t");
+ i++;
+ x[i]=p;
+ insert(x[1], x[2], x[3], temp);
+ }
+ 
+ }
+ ch++;
+ }
+ }
+ 
+ 
+ void loadDatabase(HASHTABLE temp){
+ // create a FILE typed pointer
+ FILE *file_pointer;
+ 
+ // open/create the txt file for reading/loading the database
+ file_pointer = fopen("check_data.txt","r");
+ char ch;
+ while ((ch = getc(file_pointer)) != EOF){
+ 
+ 
+ char str[] = "this, is the string - I want to parse";
+ char delim[] = " ,-";
+ char* token;
+ 
+ for (token = strtok(str, delim); token; token = strtok(NULL, delim))
+ {
+ printf("token=%s\n", token);
+ }
+ 
+ char word_loop[32];
+ char *x[4];
+ //word_loop = 3321 \t asds \t asw\n
+ for(int i = 0; ch != '\n' ; i++){
+ word_loop[i] = ch;
+ }
+ if (ch=='\n') {
+ char *p;
+ p = strtok(word_loop,"\t");
+ int i = 0;
+ x[0] = p;
+ 
+ while (strcmp(p,"\n")!=0) {
+ p = strtok(word_loop,"\t");
+ i++;
+ x[i]=p;
+ insert(x[1], x[2], x[3], temp);
+ }
+ 
+ }
+ ch++;
+ }
+ }
+ 
+ 
+ void printData(){
+ char * buffer = 0;
+ long length;
+ FILE * f = fopen ("check_data.txt", "rb");
+ 
+ if (f)
+ {
+ fseek (f, 0, SEEK_END);
+ length = ftell (f);
+ fseek (f, 0, SEEK_SET);
+ buffer = malloc (length);
+ 
+ if (buffer)
+ {
+ fread (buffer, 1, length, f);
+ }
+ fclose (f);
+ }
+ 
+ if (buffer)
+ {
+ printf("%s", buffer);
+ }
+ }
+ 
+ void parseToTokens(char str[], char delim[]){
+ char* token;
+ int i = 0;
+ printf("The number of lines are %d\n", countlines("check_data.txt"));
+ char *arr[countlines(str)];
+ 
+ for (token = strtok(str, delim); token; token = strtok(NULL, delim))
+ {
+ arr[i]=token;
+ printf("token %d  is %s\n", i, token);
+ i++;
+ }
+ 
+ //    for (int i=0; i<countlines(str); i++) {
+ //        printf("asdfasfsaf");
+ printf("The array %d is %s\n", i, arr[1]);
+ //}
+ }
+ //void justTokens(char str[], char delim[]){
+ //    char * pch;
+ //    pch = strtok (str,delim);
+ //    while (pch != NULL)
+ //    {
+ //        parseToTokens(pch, "\t");
+ //        printf ("sente %s\n",pch);
+ //        pch = strtok (NULL, delim);
+ //    }
+ //
+ //}
+ 
+ 
+ 
+ void printData(){
+ char * buffer = 0;
+ long length;
+ FILE * f = fopen ("check_data.txt", "rb");
+ 
+ if (f){
+ fseek (f, 0, SEEK_END);
+ length = ftell (f);
+ fseek (f, 0, SEEK_SET);
+ buffer = malloc (length);
+ 
+ if (buffer) {
+ fread (buffer, 1, length, f);
+ }
+ fclose (f);
+ 
+ }
+ if (buffer){
+ printf("TEXT in BUFFER\n");
+ printf("%s", buffer);
+ printf("\n\n");
+ parseToTokens(buffer, "\t\n");
+ }
+ }
+ 
+ */
+
