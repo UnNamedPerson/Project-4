9094bc6683c4d1bac15e57276a25d1e63608b5a0
UnknownPerson <UnknownPersonEmail@gmail.com>
2017-11-26 20:01:39 -0500

    Part 2.1 test

--- Part 2/Part 2/Test code.c
+++ Part 2/Part 2/Test code.c
@@ -11,144 +11,144 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define B 100
+//
+////int B = 1000;
+//
+//typedef char ETYPE[B];
+//
+//typedef struct CELL *LIST;
+//struct CELL {
+//    ETYPE element;
+//    ETYPE element1;
+//    ETYPE element2;
+//    LIST next;
+//};
+//
+//typedef LIST HASHTABLE[600];
+//
+//// Hashing Function
+//int h(ETYPE x)
+//{
+//    int i, sum;
+//
+//    sum = 0;
+//    for (i = 0; x[i] != '\0'; i++)
+//        sum += x[i];
+//    return sum % B;
+//}
+//
+//// Display Functions and other methods that're needed for display functions
+//int countNodes(LIST start){
+//    int i = 0;
+//    if (start == NULL){
+//        return i; //As nodes are 0, the list is empty
+//    } else {
+//        LIST p; p=start;
+//        while (p!=NULL){
+//            i++;
+//            p=p->next;
+//        }
+//        return i;
+//    }
+//}
+//
+//void displayCells(LIST start, int numnodes){
+//
+//    LIST p; p=start;
+//    for (int i = 0; i<(numnodes-1); i++){
+//        p=p->next;
+//        printf("\t-> {|\"%s\"| |\"%s\"| |\"%s\"|}", p->element,p->element1,p->element2);
+//    }
+//}
+//
+//void displayRelations(HASHTABLE temp){
+//    int numnodes=0;
+//    printf("\n");
+//    printf("Hash Table\t\tLists\n");
+//    LIST p;
+//    for (int i = 0; i<5; i++) {
+//        p=temp[i];
+//        if (p==NULL) {
+//            printf("HASH[%d]\t\t-> |\"%s\"|", i,temp[i]->element);
+//        } else {
+//            printf("HASH[%d]\t\t-> {|\"%s\"| |\"%s\"| |\"%s\"|}", i,p->element,p->element1,p->element2);
+//        }
+//        numnodes=countNodes(p);
+//        displayCells(p, numnodes);
+//        printf("\n");
+//    }
+//    printf("\n");
+//}
+//
+//// Insert Functions
+//int i = 0;
+//void bucketInsert(ETYPE x, ETYPE y, ETYPE z,LIST *pL)
+//{
+//    printf("recursive count: %d\n",i);
+//    i++;
+//    if ((pL) == NULL) {
+//        (*pL) = (LIST) malloc(sizeof(struct CELL));
+//        strcpy((*pL)->element, x);
+//        strcpy((*pL)->element1, y);
+//        strcpy((*pL)->element2, z);
+//        (*pL)->next = NULL;
+//    } else if (strcmp((*pL)->element, x)!=0)
+//        bucketInsert(x,y,z,&((*pL)->next));
+//    //it works also without else if statement -> in this way:
+//    // else {bucketInsert(x,y,z,&((*pL)->next));}
+//    //but book chose the first way
+//
+//}
+//void insert(ETYPE x, ETYPE y, ETYPE z,HASHTABLE H)
+//{
+//    bucketInsert(x,y,z,&(H[h(x)]));
+//}
+//
+//// Delete Functions
+//
+////if((*pL)->element!=x || (*pL)->element1!=y ||(*pL)->element2!=z){
+////still didn't get it that why  above code works it should be '==' and not '!='
+////as if it's '!=' doesn't make sense as if it's equal to x,y,or z then we should perform that remove operation
+////but if we put '==' it doesn't work, why???
+//
+//void bucketDelete(ETYPE x, ETYPE y, ETYPE z,LIST *pL)
+//{
+//    if ((*pL) != NULL) {
+//        if(strcmp((*pL)->element, x)==0 || strcmp((*pL)->element1, y)==0 ||strcmp((*pL)->element2, z)==0){
+//            (*pL)=(*pL)->next;
+//        } else {
+//            bucketDelete(x, y, z, &((*pL)->next));
+//        }
+//    }
+//}
+//void delete(ETYPE x, ETYPE y, ETYPE z,HASHTABLE S)
+//{
+//    bucketDelete(x,y,z,&(S[h(x)]));
+//}
+//
+////int main(int argc, const char * argv[]) {
+//    HASHTABLE temp;
+//
+//    printf("\n\tInserting Database\n");
+//
+//    insert("1234",  "anyd", "one",  temp);
+//    insert("5678",  "live", "edw",  temp);
+//    insert("2135",  "icid", "dar",  temp);
+//    insert("1232",  "sala", "omq",  temp);
+//    insert("8784",  "chfd", "roy",  temp);
+//    insert("3421",  "insa", "ofa",  temp);
+//    insert("4322",  "iyer", "mon",  temp);
+//
+//    displayRelations(temp);
+//
+//    printf("\n\tDeleting Some Tuples\n");
+//    
+//    delete("1232",  "sala", "omq",  temp);
+//    delete("3421",  "insa", "ofa",  temp);
+//    delete("2135",  "icid", "dar",  temp);
+//
+//    displayRelations(temp);
 
-//int B = 1000;
-
-typedef char ETYPE[B];
-
-typedef struct CELL *LIST;
-struct CELL {
-    ETYPE element;
-    ETYPE element1;
-    ETYPE element2;
-    LIST next;
-};
-
-typedef LIST HASHTABLE[600];
-
-// Hashing Function
-int h(ETYPE x)
-{
-    int i, sum;
-    
-    sum = 0;
-    for (i = 0; x[i] != '\0'; i++)
-        sum += x[i];
-    return sum % B;
-}
-
-// Display Functions and other methods that're needed for display functions
-int countNodes(LIST start){
-    int i = 0;
-    if (start == NULL){
-        return i; //As nodes are 0, the list is empty
-    } else {
-        LIST p; p=start;
-        while (p!=NULL){
-            i++;
-            p=p->next;
-        }
-        return i;
-    }
-}
-
-void displayCells(LIST start, int numnodes){
-    
-    LIST p; p=start;
-    for (int i = 0; i<(numnodes-1); i++){
-        p=p->next;
-        printf("\t-> {|\"%s\"| |\"%s\"| |\"%s\"|}", p->element,p->element1,p->element2);
-    }
-}
-
-void displayRelations(HASHTABLE temp){
-    int numnodes=0;
-    printf("\n");
-    printf("Hash Table\t\tLists\n");
-    LIST p;
-    for (int i = 0; i<5; i++) {
-        p=temp[i];
-        if (p==NULL) {
-            printf("HASH[%d]\t\t-> |\"%s\"|", i,temp[i]->element);
-        } else {
-            printf("HASH[%d]\t\t-> {|\"%s\"| |\"%s\"| |\"%s\"|}", i,p->element,p->element1,p->element2);
-        }
-        numnodes=countNodes(p);
-        displayCells(p, numnodes);
-        printf("\n");
-    }
-    printf("\n");
-}
-
-// Insert Functions
-int i = 0;
-void bucketInsert(ETYPE x, ETYPE y, ETYPE z,LIST *pL)
-{
-    printf("recursive count: %d\n",i);
-    i++;
-    if ((pL) == NULL) {
-        (*pL) = (LIST) malloc(sizeof(struct CELL));
-        strcpy((*pL)->element, x);
-        strcpy((*pL)->element1, y);
-        strcpy((*pL)->element2, z);
-        (*pL)->next = NULL;
-    } else if (strcmp((*pL)->element, x)!=0)
-        bucketInsert(x,y,z,&((*pL)->next));
-    //it works also without else if statement -> in this way:
-    // else {bucketInsert(x,y,z,&((*pL)->next));}
-    //but book chose the first way
-
-}
-void insert(ETYPE x, ETYPE y, ETYPE z,HASHTABLE H)
-{
-    bucketInsert(x,y,z,&(H[h(x)]));
-}
-
-// Delete Functions
-
-//if((*pL)->element!=x || (*pL)->element1!=y ||(*pL)->element2!=z){
-//still didn't get it that why  above code works it should be '==' and not '!='
-//as if it's '!=' doesn't make sense as if it's equal to x,y,or z then we should perform that remove operation
-//but if we put '==' it doesn't work, why???
-
-void bucketDelete(ETYPE x, ETYPE y, ETYPE z,LIST *pL)
-{
-    if ((*pL) != NULL) {
-        if(strcmp((*pL)->element, x)==0 || strcmp((*pL)->element1, y)==0 ||strcmp((*pL)->element2, z)==0){
-            (*pL)=(*pL)->next;
-        } else {
-            bucketDelete(x, y, z, &((*pL)->next));
-        }
-    }
-}
-void delete(ETYPE x, ETYPE y, ETYPE z,HASHTABLE S)
-{
-    bucketDelete(x,y,z,&(S[h(x)]));
-}
-
-//int main(int argc, const char * argv[]) {
-    HASHTABLE temp;
-    
-    printf("\n\tInserting Database\n");
-    
-    insert("1234",  "anyd", "one",  temp);
-    insert("5678",  "live", "edw",  temp);
-    insert("2135",  "icid", "dar",  temp);
-    insert("1232",  "sala", "omq",  temp);
-    insert("8784",  "chfd", "roy",  temp);
-    insert("3421",  "insa", "ofa",  temp);
-    insert("4322",  "iyer", "mon",  temp);
-    
-    displayRelations(temp);
-    
-    printf("\n\tDeleting Some Tuples\n");
-    
-    delete("1232",  "sala", "omq",  temp);
-    delete("3421",  "insa", "ofa",  temp);
-    delete("2135",  "icid", "dar",  temp);
-    
-    displayRelations(temp);
-    
 //    return 0;
 //}
 
--- Part 2/Part 2/main.c
+++ Part 2/Part 2/main.c
@@ -10,6 +10,9 @@
 
 // the tree definition for Secondary index structure
 
+typedef char ETYPE[32];
+#define B 5
+
 typedef struct tupleCGS {
     char Course[5];
     char Grade[2];
@@ -17,34 +20,204 @@ typedef struct tupleCGS {
     struct tupleCGS *next;
 }CGSLIST;
 
+typedef struct CELL *LIST;
+struct CELL {
+    ETYPE element;
+    ETYPE element1;
+    ETYPE element2;
+    LIST next;
+};
+
+typedef LIST HASHTABLE[30];
+
+// Hashing Function
+int h(ETYPE x)
+{
+    int i, sum;
+    
+    sum = 0;
+    for (i = 0; x[i] != '\0'; i++)
+        sum += x[i];
+    return sum % B;
+}
+
+int SumOfChars(char* string){
+    int Sum = 0;
+    int i=0;
+    //    int size = strlen(string);
+    //    printf("the size of %s is %d\n", string, strlen(string));
+    while(string[i] != '\0'){
+        Sum = Sum + string[i];
+        //        string++;
+        i++;
+    }
+    return Sum;
+}
+
+
 typedef struct Node *SITree;
 struct Node {
-    char Name[30]; // or depending on the what is decided as the 2nd index
-    CGSLIST PointertoCGS;
+    char* Name; // or depending on the what is decided as the 2nd index
+    LIST PointertoList;
+    SITree Next;
     SITree Lc;
     SITree Rc;
 };
+typedef SITree SINodeList[100];
 
-void insertTree(SITree SecondIndex, char name[30], CGSLIST p){
-    if(SecondIndex == NULL){
+
+SITree newSINode(char* name, LIST PointertoList){
+    SITree temp =  (SITree )malloc(sizeof(struct Node));
+    temp->Name = name;
+    temp->Lc = temp->Rc = NULL;
+    temp->PointertoList = PointertoList;
+    return temp;
+}
+
+void bucketLookup(ETYPE x, ETYPE y, ETYPE z, LIST *pL){
+    HASHTABLE* pLTable; // the return variable table.
+    
+    if(strcmp((*pL)->element, x)==0)// if the Studnetid is found
+    {
+        if(strcmp(y, "*")==0 && strcmp(z, "*")==0){
+            while((*pL) != NULL){
+                printf("StudentID: %s |Course :%s| Grade: %s\n", (*pL)->element, (*pL)->element1, (*pL)->element2);
+//                pLTable[0]->element = (*pL)->element;
+//                pLTable[0]->element1 = (*pL)->element1;
+//                pLTable[0]->element2 = (*pL)->element2;
+                
+                //                pLTable[0]->
+                (*pL)=(*pL)->next;
+            }
+        } else if(strcmp(y, "*")==0){
+            while( (*pL) != NULL){ // TO DO: check the condition
+                if(strcmp((*pL)->element2, z)==0){
+                    printf("StudentID: %s |Course :%s| Grade: %s\n",  (*pL)->element,  (*pL)->element1, (*pL)->element2);
+                }
+                (*pL)=(*pL)->next;
+            }
+        } else if(strcmp(z, "*")==0){
+            while((*pL) != NULL){
+                if(strcmp((*pL)->element1, y)==0){
+                    printf("StudentID: %s |Course :%s| Grade: %s\n", (*pL)->element, (*pL)->element1, (*pL)->element2);
+                }
+                (*pL)=(*pL)->next;
+            }
+        } else {
+            printf("StudentID: %s |Course :%s| Grade: %s\n", (*pL)->element, (*pL)->element1, (*pL)->element2);
+        }
         
-    }
-    else{
-        if(SecondIndex->Name > name){
-            
-        }
-        else{
-            
-        }
+    } else {
+        bucketLookup(x, y, z, &((*pL)->next));
     }
 }
 
-// updating the secondary index
-void update(){
+SITree SIinsert(SITree node, char* name, LIST PointertoList){
+    // If the SItree is empty, return a new SInode
+    if (node == NULL){
+        return newSINode(name, PointertoList);
+    }
+    // Otherwise, recur down the SItree
+    else{
+        if (SumOfChars(name) < SumOfChars(node->Name)){
+            node->Lc  = SIinsert(node->Lc, name, PointertoList);
+        }
+        // This is a special case for when two or more students have the same name
+        else if(SumOfChars(node->Name) == SumOfChars(name)){
+            node->Next = newSINode(name, PointertoList);
+        }
+        else if (SumOfChars(name) > SumOfChars(node->Name)){
+            node->Rc = SIinsert(node->Rc, name, PointertoList);
+        }
+    }
+    
+    return node;
+}
+
+
+SITree search(SITree root, char* name){
+//    SINodeList tempList;
+
+    if (root == NULL || strcmp(root->Name, name) == 0)
+        return root;
+    
+    // name characters values are greater than root's Name
+    if ( SumOfChars(root->Name)  < SumOfChars(name) ){
+        return search(root->Rc, name);
+    }
+    
+    // name characters values are smaller than root's Name
+    else{
+    return search(root->Lc, name);
+    }
+}
+
+//void insertTree(SITree SecondIndex, char name[30], CGSLIST p){
+//    if(SecondIndex == NULL){
+//
+//    }
+//    else{
+//        if(SecondIndex->Name > name){
+//
+//        }
+//        else{
+//
+//        }
+//    }
+//}
+
+
+
+void bucketInsert(ETYPE x, ETYPE y, ETYPE z,LIST *pL, SITree Root)
+{
+    if ((*pL) == NULL) {
+        (*pL) = (LIST) malloc(sizeof(struct CELL));
+        strcpy((*pL)->element, x);
+        strcpy((*pL)->element1, y);
+        strcpy((*pL)->element2, z);
+        (*pL)->next = NULL;
+        SIinsert(Root, (*pL)->element1, pL);
+    } else if (strcmp((*pL)->element, x))
+        bucketInsert(x,y,z,&((*pL)->next),Root);
+    
+}
+void insert(ETYPE x, ETYPE y, ETYPE z,HASHTABLE H, SITree Root)
+{
+    bucketInsert(x,y,z,&(H[h(x)]), Root);
+}
+
+void StudentGradeQuery(char* element1, char* course, SITree Root){
+    LIST P = (LIST)malloc(sizeof(LIST));
+    strcpy(P->element, "*");
+    strcpy(P->element1, element1);
+    strcpy(P->element2,"*");
+
+    SITree temp;
+    
+    temp = search(Root, element1);
+    
+    bucketLookup(P->element, temp->Name,P->element2 , &P) ;
     
 }
 
+// updating the secondary index
+//void update(){
+//
+//}
+
 int main(int argc, const char * argv[]) {
+    
+    SITree Root;
+    HASHTABLE H;
+    
+    insert("Islom", "0121313", "element1", H, Root);
+    insert("Islam", "1990021", "hobb", H, Root);
+    insert("Ahmad", "8141232", "herk", H, Root);
+    insert("Abdullah", "3999183", "wow", H, Root);
+    insert("Sayari", "38951993", "y", H, Root);
+
+    
+    StudentGradeQuery("Islam", "Jerk", Root);
 
     return 0;
 }
